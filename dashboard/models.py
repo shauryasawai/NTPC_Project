a0730775d3af121d69d49c76f280bae2388c77cb
Shauryaman Sawai <sawaisushil@gmail.com>
2025-03-01 17:01:47 +0530

    attempt_1

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,24 @@
+
 from django.db import models
 
-# Create your models here.
+class SustainabilityReport(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    total_energy_generated = models.FloatField()  # MW
+    fossil_fuel_offset = models.FloatField()  # MW replaced by renewable energy
+    co2_reduction = models.FloatField()  # kg of CO2 saved
+
+    def __str__(self):
+        return f"CO2 Reduction Report - {self.timestamp}"
+
+class GridBalance(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    energy_demand = models.FloatField()  # MW
+    energy_supply = models.FloatField()  # MW
+    balance = models.FloatField()  # Difference (positive = surplus, negative = shortage)
+
+    def save(self, *args, **kwargs):
+        self.balance = self.energy_supply - self.energy_demand
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Grid Balance - {self.timestamp}"
--- dashboard/views.py
+++ dashboard/views.py
@@ -16,3 +16,49 @@ def map_view(request):
     # Pass sensor data for map visualization
     sensors = SensorData.objects.filter(failure_risk__gt=0.30)
     return render(request, "dashboard/map.html", {"sensors": sensors})
+
+from django.shortcuts import render
+from .models import SustainabilityReport, GridBalance
+from .utils import calculate_co2_savings, update_grid_balance
+
+def analytics_dashboard(request):
+    # Update data
+    calculate_co2_savings()
+    update_grid_balance()
+
+    # Fetch latest reports
+    sustainability_reports = SustainabilityReport.objects.all().order_by('-timestamp')[:10]
+    grid_balances = GridBalance.objects.all().order_by('-timestamp')[:10]
+
+    return render(request, "dashboard/analytics.html", {
+        "sustainability_reports": sustainability_reports,
+        "grid_balances": grid_balances
+    })
+
+from django.http import JsonResponse
+
+def sustainability_report_api(request):
+    reports = SustainabilityReport.objects.all().order_by('-timestamp')[:10]
+    data = [
+        {
+            "timestamp": str(report.timestamp),
+            "total_energy_generated": report.total_energy_generated,
+            "fossil_fuel_offset": report.fossil_fuel_offset,
+            "co2_reduction": report.co2_reduction
+        }
+        for report in reports
+    ]
+    return JsonResponse({"sustainability_reports": data})
+
+def grid_balance_api(request):
+    balances = GridBalance.objects.all().order_by('-timestamp')[:10]
+    data = [
+        {
+            "timestamp": str(balance.timestamp),
+            "energy_demand": balance.energy_demand,
+            "energy_supply": balance.energy_supply,
+            "balance": balance.balance
+        }
+        for balance in balances
+    ]
+    return JsonResponse({"grid_balance": data})
