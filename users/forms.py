867100f0b1de7459da0d37b732dfae54a736fa9b
Shauryaman Sawai <sawaisushil@gmail.com>
2025-03-01 19:06:18 +0530

    registration

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,18 @@ from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
     role = forms.ChoiceField(choices=CustomUser.ROLE_CHOICES)
-
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'dark-input'}),
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'dark-input'}),
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'dark-input', 'help_text': ''}),
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'dark-input', 'help_text': ''}),
+    )
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'role', 'password1', 'password2']
